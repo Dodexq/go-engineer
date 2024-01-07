17d8bcb09ae9e4ece72892933442224de62e61e6
Dodexq <dodexpwnz@gmail.com>
2024-01-07 21:13:02 +0300

    interfaces

--- deep/lesson/interfaces.go
+++ deep/lesson/interfaces.go
@@ -12,9 +12,18 @@ type Sender interface {
 	Send(msg string) error
 }
 
+type Email struct {
+	Address string
+}
+
 type Phone struct {
 	Number  int
 	Balance float64
+}
+
+func (e *Email) Send(msg string) error {
+	fmt.Printf("На адрес %v было направлено письмо", msg)
+	return nil
 }
 
 func (p *Phone) Call(num int) error {
@@ -31,22 +40,26 @@ func Notify(i interface{}) {
 	switch i.(type) {
 	case int:
 		fmt.Println("Int не поддерживается")
+	case *Email:
+		mail, ok := i.(*Email)
+		if ok {
+			fmt.Println("Отправлен на электронную почту -", mail.Address)
+		}
+		fmt.Println("Success!")
+	case *Phone:
+		phone, ok := i.(*Phone)
+		if ok {
+			fmt.Printf("Отправлен по телефону %v\n", phone.Number)
+			fmt.Printf("Баланс составляет %v$", phone.Balance)
+		}
+		fmt.Println("Success!")
 	}
-
-	s, ok := i.(Sender)
-	if !ok {
-		fmt.Println("Ошибка пустого интерфейса")
-		return
-	}
-	err := s.Send("Сообщение пустого интерфейса")
-	if err != nil {
-		fmt.Println("Ошибка")
-	}
-	fmt.Println("Sucess")
-
 }
 
 func main() {
-	Notify(&Phone{Number: 23})
+	e := &Email{Address: "dodexpwnz@gmail.com"}
+	Notify(e)
 
+	p := &Phone{Number: 95210706, Balance: 120}
+	Notify(p)
 }
